4c0ec300f97eb45e57138e0c87adf6dcd740509a
George <geo.rog09@gmail.com>
2020-05-02 16:55:16 +0100

    Update

--- src/Driver.java
+++ src/Driver.java
@@ -1,20 +1,13 @@
 public class Driver {
 
     public static void main(String[] args) {
-
         Exponentiation exp = new Exponentiation();
-
         double base = 3.0;
         int exponent = 15;
-
         double result = exp.binaryExponentiation(base, exponent);
         double correctResult = Math.pow(base, exponent);
-
         int numberOfMultiplications = exp.binaryMultiplicationCount(exponent);
-
         System.out.println("Result: " + result + " (Correct result = "+ correctResult + ")");
         System.out.println("Number of multiplications needed: " + numberOfMultiplications);
-
     }
-
 }
